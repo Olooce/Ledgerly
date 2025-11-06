8ad2044408c869cda1257936f4ca5f386a5b3f02
Olooce <121952433+Olooce@users.noreply.github.com>
2025-11-06 19:56:49 +0300

    docs: update README.md

--- app/src/main/java/ke/ac/ku/ledgerly/auth/di/AuthModule.kt
+++ app/src/main/java/ke/ac/ku/ledgerly/auth/di/AuthModule.kt
@@ -3,9 +3,6 @@ package ke.ac.ku.ledgerly.auth.di
 import android.content.Context
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
